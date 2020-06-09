e7d086da4946551e9b1067b528edc46f2d95131f
claudiobailon <c.bailonschubert@gmail.com>
2020-06-09 08:53:20 -0700

    Moved random number function outside of object

--- app.js
+++ app.js
@@ -1,27 +1,50 @@
 'use strict';
 
-var locationSeattle = {
+
+
+function getRandomNumber(min,max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+}
+
+var seattleLocation = {
   minCustomers: 23,
   maxCustomers: 65,
   avgCookie: 6.3,
-  getCustomersPerHour: function(){
-    return Math.floor((Math.random() * (this.maxCustomers-this.minCustomers)) + this.minCustomers);
-  }, // used this page to figure out how to produce a random number https://www.geeksforgeeks.org/how-to-generate-random-number-in-given-range-using-javascript/
+  customersPerHour = getRandomNumber(this.minCustomers, this.maxCustomers),
+
+  // getCustomersPerHour: function(){
+
+  //   return Math.floor((Math.random() * (this.maxCustomers-this.minCustomers)) + this.minCustomers);
+  //   // used this page to figure out how to produce a random number https://www.geeksforgeeks.org/how-to-generate-random-number-in-given-range-using-javascript/
+  // }, 
 
  getSales: function (){
+  var sales = this.customersPerHour * this.avgCookie;
+  return Math.round(sales);
+  
+  },
 
-  return Math.round(locationSeattle['getCustomersPerHour']() * locationSeattle.avgCookie)
- },
- 
+salesData: [this.avgCookie, this.customersPerHour, this.getSales],
 
- salesData: [this.avgCookie, this.getCustomersPerHour, this.getSales],
+renderToPage : function(){
+  
+  var parentUnorderedList = document.getElementById('seattle-ul');
+  var listItem = document.createElement('li');
+  listItem.textContent= seattleLocation.getSales() ;
+  parentUnorderedList.appendChild(listItem);
+  
+  }
 };
 
-
-console.log('max customers: '+ locationSeattle['maxCustomers']);
-console.log('Avg Customers Per Hour: ' + locationSeattle['getCustomersPerHour']());
-console.log('Cookie Sales: ' + locationSeattle['getSales']())
-console.log(locationSeattle['salesData']);
-//console.log(locationSeattle['getRandomNumber'](locationSeattle.minCustomers, locationSeattle.maxCustomers));
+seattleLocation.renderToPage();
+// console.log('max customers: '+ seattleLocation['maxCustomers']);
+console.log('Avg Cookies per cutomer: ', seattleLocation.avgCookie);
+console.log('Avg Customers Per Hour: ' + seattleLocation['customersPerHour']);
+//console.log('sales: ',sales)
+console.log('Cookie Sales: ' + seattleLocation['getSales']())
+console.log(seattleLocation.salesData);
+//console.log(seattleLocation['getRandomNumber'](seattleLocation.minCustomers, seattleLocation.maxCustomers));
 
 
